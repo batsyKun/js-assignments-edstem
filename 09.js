01dd6a2ab248710483e92cc35906230f65cd51bf
BatsyKun <119741750+KoochieKun@users.noreply.github.com>
2023-07-12 13:01:11 +0530

    09 complete

---
+++ 09.js
@@ -0,0 +1,7 @@
+let sizeCode = "M";
+
+sizeCode === "S" ? console.log("Small") :
+  sizeCode === "M" ? console.log("Medium") :
+    sizeCode === "L" ? console.log("Large") :
+        sizeCode === "XL" ? console.log("Extra Large") :
+  console.log("None");
