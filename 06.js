efdad1981022b0a5771fe87fa152bb2dffe1aa64
BatsyKun <119741750+KoochieKun@users.noreply.github.com>
2023-07-12 12:12:42 +0530

    06 complete

---
+++ 06.js
@@ -0,0 +1,9 @@
+const item1 = 300;
+const item2 = 600;
+const item3 = 1100;
+
+let item1Quantity = 3;
+let item2Quantity = 2;
+let item3Quantity = 1;
+
+totalItems = item1Quantity+item2Quantity+item3Quantity;
